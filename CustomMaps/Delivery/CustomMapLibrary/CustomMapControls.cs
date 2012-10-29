b5dfde62cdcefc0d74bfa42adc8c50ed930e35ed
Anubhav Gupta <anubhavgupta.work@gmail.com>
2012-10-29 20:03:54 +0530

    Updated Custom Maps Library

---
+++ CustomMaps/Delivery/CustomMapLibrary/CustomMapControls.cs
@@ -0,0 +1,241 @@
+﻿using System;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Ink;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+using System.Device.Location;
+using Microsoft.Phone.Controls.Maps;
+using System.Windows.Threading;
+using System.Threading;
+using System.Windows.Media.Imaging;
+using System.Windows.Controls.Primitives;
+
+
+namespace CustomMapLibrary
+{
+    /// <summary>
+    ///Custom Maps adds following capabilities to a Map.
+    ///1)Show Default location.
+    ///2)Redirect to default location.
+    ///3)Pin drop on long tap.
+    ///4)Custom popup.
+    /// </summary>
+    public class CustomMapControls
+    {
+        private GeoCoordinate _defaultLocation = new GeoCoordinate();
+        private GeoCoordinate _customLocation = new GeoCoordinate();
+        private GeoCoordinateWatcher _watcher;
+        private Map _map;
+
+        /// <summary>
+        /// Represents a map layer which positions it's child(UIElement) using geographic coordinates.
+        /// </summary>
+        public MapLayer ImageLayer = new MapLayer();
+
+        private Image _homeImage = new Image();
+        private Image _pinImage = new Image();
+
+        private PopupWindow _popupWindow = new PopupWindow();
+        private double _click_X, _click_Y;
+
+        public delegate void Popup_Button_Clicked(object sender, RoutedEventArgs args);
+
+        /// <summary>
+        /// Occurs when button on popup window is clicked.
+        /// </summary>
+        public event Popup_Button_Clicked popupClicked;
+
+        /// <summary>
+        /// Set Or Get Zoom Level For Default Location.
+        /// </summary>
+        public double ZoomLevel
+        {
+            get;
+            set;
+        }
+
+        /// <summary>
+        /// Creates a new CustomMapControls class object using default images.
+        /// </summary>
+        /// <param name="map">Map object on which actions are to be performed. </param>
+        public CustomMapControls(Map map)
+        {
+            try
+            {
+                this._map = map;
+                _map.Children.Add(ImageLayer);
+                _homeImage.Source = new BitmapImage(new Uri("/CustomMapLibrary;component/Resources/home.png", UriKind.Relative));
+                _pinImage.Source = new BitmapImage(new Uri("/CustomMapLibrary;component/Resources/pin.png", UriKind.Relative));
+
+                _pinImage.Tap += new EventHandler<GestureEventArgs>(pinImage_Tap);
+                _map.Tap += new EventHandler<GestureEventArgs>(map_Tap);
+                _map.Hold += new EventHandler<GestureEventArgs>(map_Hold);
+                _watcher = new GeoCoordinateWatcher(GeoPositionAccuracy.High);
+                _watcher.MovementThreshold = 20;
+                _watcher.PositionChanged += new EventHandler<GeoPositionChangedEventArgs<GeoCoordinate>>(watcher_PositionChanged);
+                _watcher.StatusChanged += new EventHandler<GeoPositionStatusChangedEventArgs>(watcher_StatusChanged);
+                _watcher.Start(true);
+
+                //Forward Button Press Event from Popup Window 
+                _popupWindow.btnDetails.Click += new RoutedEventHandler(btnDetails_Click);
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show("Error: " + e.Message + "\n" + e.Data + "\n" + e.StackTrace);
+            }
+        }
+
+        /// <summary>
+        /// Creates a new CustomMapControls class object using supplied image source.
+        /// </summary>
+        /// <param name="map">Map object on which actions are to be performed. </param>
+        /// <param name="defaultLocationImage">Default or Home Location Image</param>
+        /// <param name="customLocationImage">Custom Location Image</param>
+        public CustomMapControls(Map map, ImageSource defaultLocationImage, ImageSource customLocationImage)
+        {
+            try
+            {
+                this._map = map;
+                _map.Children.Add(ImageLayer);
+                _homeImage.Source = defaultLocationImage;
+                _pinImage.Source = customLocationImage;
+
+                _pinImage.Tap += new EventHandler<GestureEventArgs>(pinImage_Tap);
+                _map.Tap += new EventHandler<GestureEventArgs>(map_Tap);
+                _map.Hold += new EventHandler<GestureEventArgs>(map_Hold);
+                _watcher = new GeoCoordinateWatcher(GeoPositionAccuracy.High);
+                _watcher.MovementThreshold = 20;
+                _watcher.PositionChanged += new EventHandler<GeoPositionChangedEventArgs<GeoCoordinate>>(watcher_PositionChanged);
+                _watcher.StatusChanged += new EventHandler<GeoPositionStatusChangedEventArgs>(watcher_StatusChanged);
+                _watcher.Start(true);
+
+                //Forward Button Press Event from Popup Window 
+                _popupWindow.btnDetails.Click += new RoutedEventHandler(btnDetails_Click);
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show("Error: " + e.Message + "\n" + e.Data + "\n" + e.StackTrace);
+            }
+        }
+
+        //forward event handling....
+        void btnDetails_Click(object sender, RoutedEventArgs e)
+        {
+            if (popupClicked != null)
+            {
+                popupClicked(sender, e);
+            }
+        }
+
+        //removing popup
+        void map_Tap(object sender, GestureEventArgs e)
+        {
+            //Discards any tap made on the customLocation's pin.
+            //(Tap Event sequence -> (1)pinImage -> (2)map)
+            if (!((_click_X == e.GetPosition(_map).X) && (_click_Y == e.GetPosition(_map).Y)))
+            {
+                ImageLayer.Children.Remove(_popupWindow);
+            }
+
+        }
+
+        //adding popup to screen
+        //(Tap Event sequence-> (1)pinImage -> (2)map)
+        void pinImage_Tap(object sender, GestureEventArgs e)
+        {
+            try
+            {
+                System.Diagnostics.Debug.WriteLine("clicked");
+                _click_X = e.GetPosition(_map).X;
+                _click_Y = e.GetPosition(_map).Y;
+
+                ImageLayer.Children.Remove(_popupWindow);
+                ImageLayer.AddChild(_popupWindow, _customLocation, PositionOrigin.BottomRight);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error: " + ex.Message + "\n" + ex.Data + "\n" + ex.StackTrace);
+            }
+
+        }
+
+        //adding custom pin to screen
+        void map_Hold(object sender, GestureEventArgs e)
+        {
+            try
+            {
+                _pinImage.MaxHeight = 50;
+                _pinImage.MaxWidth = 50;
+                _pinImage.Opacity = 0.8;
+                ImageLayer.Children.Remove(_pinImage);
+                _customLocation = _map.ViewportPointToLocation(e.GetPosition(_map));
+                ImageLayer.AddChild(_pinImage, _customLocation, PositionOrigin.BottomCenter);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error: " + ex.Message + "\n" + ex.Data + "\n" + ex.StackTrace);
+            }
+        }
+
+
+        void watcher_StatusChanged(object sender, GeoPositionStatusChangedEventArgs e)
+        {
+            switch (e.Status)
+            {
+                case GeoPositionStatus.Disabled:
+
+                case GeoPositionStatus.NoData: MessageBox.Show("GPS Disabled or internet not available");
+                    break;
+                case GeoPositionStatus.Initializing:
+
+                case GeoPositionStatus.Ready: break;
+            }
+        }
+
+        //Updates default Position.
+        void watcher_PositionChanged(object sender, GeoPositionChangedEventArgs<GeoCoordinate> e)
+        {
+            try
+            {
+                _defaultLocation.Latitude = e.Position.Location.Latitude;
+                _defaultLocation.Longitude = e.Position.Location.Longitude;
+                _homeImage.MaxHeight = 50;
+                _homeImage.MaxWidth = 50;
+                _homeImage.Opacity = 0.8;
+                ImageLayer.Children.Remove(_homeImage);
+                ImageLayer.AddChild(_homeImage, _defaultLocation, PositionOrigin.Center);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error: " + ex.Message + "\n" + ex.Data + "\n" + ex.StackTrace);
+            }
+
+        }
+
+        /// <summary>
+        /// Navigate View to current GPS Location with Default Zoom Level.
+        /// Use Property "ZoomLevel" To set or get default Location's Zoom Level.
+        /// </summary>
+        public void SetViewDefaultLocation()
+        {
+            _map.SetView(_defaultLocation, ZoomLevel);
+        }
+
+
+        /// <summary>
+        /// Navigate View to current GPS Location with Passed Zoom Level.
+        /// </summary>
+        /// <param name="zoomLevel">Zoom Level </param>
+        public void SetViewDefaultLocation(double zoomLevel)
+        {
+            _map.SetView(_defaultLocation, zoomLevel);
+        }
+
+
+    }
+}
---
+++ CustomMaps/Delivery/CustomMapLibrary/ZoomScroll.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Ink;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+using System.Device.Location;
+using Microsoft.Phone.Controls.Maps;
+using System.Windows.Threading;
+
+
+
+
+namespace CustomMapLibrary
+{
+    /// <summary>
+    /// Give Access to ZoomScrollChanged event to handle zoom or scroll action performed on the map.
+    /// </summary>
+    public class ZoomScroll
+    {
+        public delegate void ZoomScrollChanged(object sender,ZoomScrollArgs args);
+        /// <summary>
+        /// Occurs when map is zoomed or scrolled.
+        /// </summary>
+        public event ZoomScrollChanged zoomScrollChanged;
+        private ZoomScrollArgs _args = new ZoomScrollArgs();
+        private DispatcherTimer _timer;
+        private int _secondsCount = 0;
+        private Map _map;
+
+        private bool _changedByEvents = false;
+
+        private double _oldZoomLevel=-1, _newZoomLevel;
+        /// <summary>
+        /// Creates an object of ZoomScroll Class.
+        /// </summary>
+        /// <param name="map">Map Class Object</param>
+        public ZoomScroll(Map map)
+        {
+            try
+            {
+                this._map = map;
+                _map.MapPan += new EventHandler<MapDragEventArgs>(map_MapPan);
+                _map.MapZoom += new EventHandler<MapZoomEventArgs>(map_MapZoom);
+                _map.MapResolved += new EventHandler(map_MapResolved);
+                _timer = new DispatcherTimer();
+                _timer.Interval = TimeSpan.FromSeconds(1);
+                _timer.Tick += new EventHandler(timer_Tick);
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show("Error:"+e.Message+"\n"+e.Data+"\n"+e.StackTrace);
+            }
+        }
+
+        
+
+        //Event firing
+        void timer_Tick(object sender, EventArgs e)
+        {
+            _secondsCount++;
+            if (_secondsCount >= 3)
+            {
+                _secondsCount = 0;
+                _timer.Stop();
+                if (_args.ZoomOrScroll == ZoomScrollType.Scroll)
+                {
+                    if (zoomScrollChanged != null)
+                    {
+                        zoomScrollChanged(this, _args);
+                    }
+                }
+                else
+                {
+                    if (zoomScrollChanged != null)
+                    {
+                        zoomScrollChanged(this, _args);
+                    }
+                }
+            
+            }
+        }
+
+
+        
+        void map_MapZoom(object sender, MapZoomEventArgs e)
+        {
+            _timer.Stop();
+            _args.ZoomOrScroll = ZoomScrollType.Zoom;
+            _args.ZoomLevel = _map.ZoomLevel;
+            _args.Center = _map.ViewportPointToLocation(e.ViewportPoint);
+            _changedByEvents = true;
+        }
+
+        
+        void map_MapPan(object sender, MapDragEventArgs e)
+        {
+            _timer.Stop();
+            _args.ZoomOrScroll = ZoomScrollType.Scroll;
+            _args.Center = _map.ViewportPointToLocation(e.ViewportPoint);
+            _changedByEvents = true;
+        }
+
+        void map_MapResolved(object sender, EventArgs e)
+        {
+            _timer.Stop();
+            if (!_changedByEvents)
+            {
+                _newZoomLevel = _map.ZoomLevel;
+                if (_oldZoomLevel != _newZoomLevel)
+                {
+                    _args.ZoomLevel = _newZoomLevel;
+                    _args.ZoomOrScroll = ZoomScrollType.Zoom;
+                    _args.Center = _map.Center;
+                }
+                else
+                {
+                    _args.ZoomOrScroll = ZoomScrollType.Scroll;
+                    _args.Center = _map.Center;
+                }
+            }
+            _changedByEvents = false;
+            _oldZoomLevel = _newZoomLevel;
+            _timer.Start();
+
+
+        }
+
+    }
+}
